e121c7f4f9ed18fdbf365316bae6a7472002920a
michaelmirhom <micharby73@gmail.com>
2023-05-28 20:08:58 -0500

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
 import React from "react";
+import Home from "./Home";
+import About from "./About";
+import ProjectItem from "./ProjectItem";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+ const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+    
+ <a key={Home.id} href="#home">home</a>
+ <a key={About.id} href="#about">about</a>
+ <a key={ProjectItem.id} href="#projects">projects</a>
+  
+   
+            
+  
+  </nav>);
 }
 
 export default NavBar;
+
